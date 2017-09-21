74ead3638295a7d3ee0294af8e280697a01c8e75
Patrick Ring <ringpatrick89@gmail.com>
2017-09-21 22:30:42 +0200

    Render Subscriber table to main page

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,18 +1,21 @@
-console.log('main.js loaded')
+'use strict';
 
-$('#sendsms').on('click', function (event) {
-    event.preventDefault();
-    var element = $(this);
-    console.log('button clicked')
-
-    sendRequest(element);
-})
 
 function sendRequest(element) {
     $.ajax({
         url: '/get_subscribers/',
         type: 'GET',
         //data: element.attr('data-id')
+        async: true,
+        beforeSend: function () {
+            //$('#ajaxGif').show();
+        },
+        complete: function () {
+            //$('#ajaxGif').hide();
+        },
+        error: function () {
+            alert("Request has failed");
+        },
         success: function (response) {
             var o = JSON.parse(response);
             console.log(o);
@@ -26,15 +29,53 @@ function renderAllSubscribers(subs) {
     // test print IMSI
     console.log(subs[1][3]);
 
-    // for (var sub in subs) {
-    //     $('#subs').append('<p>' + sub + '</p>');
-    // }
+    var subsL = subs.length,
+        indices = [0,3,5],
+        tbody = document.createElement('tbody'),
+        tr,
+        td;
 
-    for (var i = 0; i < subs.length; i++) {
-        $('#subs').append('<div id=" "+subs[i][1]+" ">' + subs[i][3] + '</div>');
+
+    for (var i = 0; i < subsL; i++) {
+
+        var sub = subs[i];
+        var id = sub[0];
+        var imsi = sub[3];
+        var number = sub[5]
+
+        var strImsi = ' IMSI: ' + imsi;
+        var strId = ' Id: ' + id;
+        var strNumber = ' Extension: ' + number;
+
+        tr = document.createElement('tr');
+
+        for (var c = 0; c < 3; c++) {
+            td = document.createElement('td');
+            tr.appendChild(td);
+            td.innerHTML = sub[indices[c]];
+        }
+        tbody.appendChild(tr);
+
+
+        //$('#subs').append('<div id="\'' + id + '\'"></div>');
+
+        //  $('#' + id).append('<div class="" id="\'' + id + '\'">' + strId + '</div>');
+
+
+        //html += '<div class="" id="\'' + id + '\'">' + strId + strImsi + strNumber + '</div>';
+
     }
-    // res.forEach(function (entry) {
-    //     //console.log(res);
-    //     $('#subs').append('<p>' + entry + '</p>');
-    // })
+    document.getElementById('substable').appendChild(tbody);
 }
+
+
+$('#sendsms').on('click', function (event) {
+    event.preventDefault();
+    var element = $(this);
+    console.log('button clicked');
+
+    sendRequest(element);
+});
+
+
+console.log('main.js loaded')
